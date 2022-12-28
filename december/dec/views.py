18ef20564d33a1181acb1a22b361d28ff7e47a7b
BabintsevVO <babintsevvo@gmail.com>
2022-12-28 17:50:25 +0300

    add pagination, posts

--- december/dec/views.py
+++ december/dec/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post, Category, Tag
 
-from django.http import HttpResponse
+
+class Home(ListView):
+    model = Post
+    template_name = 'dec/index.html'
+    context_object_name = 'posts'
+    paginate_by = 1
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Classic blog design'
+        return context
 
 
 def index(request):
     return render(request, 'dec/index.html')
 
+
 def get_category(request, slug):
+    return render(request, 'dec/category.html')
+
+
+def get_post(request, slug):
     return render(request, 'dec/category.html')
