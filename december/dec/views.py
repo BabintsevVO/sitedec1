8769c70d980370645743f7e6bc374dc75bc9adf6
BabintsevVO <babintsevvo@gmail.com>
2022-12-28 18:39:38 +0300

    add posts_by_category, posts_by_tag, open one post

--- december/dec/views.py
+++ december/dec/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post, Category, Tag
+from django.db.models import F
 
 
 class Home(ListView):
@@ -15,13 +16,43 @@ class Home(ListView):
         return context
 
 
-def index(request):
-    return render(request, 'dec/index.html')
+class PostsByCategory(ListView):
+    template_name = 'dec/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
+        return context
 
 
-def get_category(request, slug):
-    return render(request, 'dec/category.html')
+class PostsByTag(ListView):
+    template_name = 'dec/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
 
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
 
-def get_post(request, slug):
-    return render(request, 'dec/category.html')
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Записи по тегу: ' + str(Tag.objects.get(slug=self.kwargs['slug']))
+        return context
+
+class GetPost(DetailView):
+    model = Post
+    template_name = 'dec/single.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.views = F ('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
+        return context
