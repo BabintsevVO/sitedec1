0d2124e208c4ab04fc999192a7330026e3f400cb
BabintsevVO <babintsevvo@gmail.com>
2022-12-29 13:13:03 +0300

    add templatetag tags_tpl, Search

--- december/dec/views.py
+++ december/dec/views.py
@@ -8,7 +8,7 @@ class Home(ListView):
     model = Post
     template_name = 'dec/index.html'
     context_object_name = 'posts'
-    paginate_by = 1
+    paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -56,4 +56,18 @@ class GetPost(DetailView):
         self.object.views = F ('views') + 1
         self.object.save()
         self.object.refresh_from_db()
+        return context
+
+
+class Search(ListView):
+    template_name = 'dec/search.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_queryset(self):
+        return Post.objects.filter(title__icontains=self.request.GET.get('s'))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['s'] = f"s={self.request.GET.get('s')}&"
         return context
